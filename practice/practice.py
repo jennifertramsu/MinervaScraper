59721267e95b81a317cd04cd78d43a907c9f23e4
jennifertramsu <jennifer.tramsu@mail.mcgill.ca>
2021-06-04 23:06:35 -0400

    Will practice with a site I actually care about

--- practice/practice.py
+++ practice/practice.py
@@ -11,6 +11,17 @@ URL = 'https://www.monster.com/jobs/search/?=Software-Developer&where=Australia'
 page = requests.get(URL)
 
 # creating the Beautiful Soup object
-soup = BeautifulSoup(page.content, 'html-parser')
+soup = BeautifulSoup(page.content, 'lxml')
+
+print(soup.title)
+print(soup.title.name) # pritns tag (title)
+print(soup.title.string) # prints webpage name
+print(soup.title.parent.name) # prints tag of parent structure (head)
+# print(soup.div.prettify())
+# print(soup.find_all('a')) # returns a list containing all HTML structures inside the inputed tag
 
 # the id attribute maeks the element uniquely identifiable on the page
+# other identifiers include class and name
+results = soup.find(class_='results-page container')
+
+print(results.prettify())
