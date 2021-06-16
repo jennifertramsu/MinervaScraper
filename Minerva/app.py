190516ce6a8831c058125859eee858a496271cb8
jennifertramsu <jennifer.tramsu@mail.mcgill.ca>
2021-06-16 00:27:58 -0400

    WHY

--- Minerva/app.py
+++ Minerva/app.py
@@ -6,19 +6,13 @@ app = Flask('')
 
 @app.route('/')
 def main():
-"""    server = Thread(target=job)
+    server = Thread(target=job)
     server.start()
-    server.join()"""
+    server.join()
     return "Minerva update is running!"
 
 def run():
     app.run(host="0.0.0.0", port=8080)
-'''
-def keep_alive():
-    server = Thread(target=run)
-    server.daemon = True
-    server.start()
-'''
 
 def job():
     os.system('python gradeupdate.py')
